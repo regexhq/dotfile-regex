3537eec80cdd9a42e64c2449a11d09e0f97338ee
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-30 11:00:23 -0400

    ensure at least one non-dot, not-slash character follows dot

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /*!
  * dotfile-regex <https://github.com/regexps/dotfile-regex>
  *
- * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Copyright (c) 2015-2017, Jon Schlinkert.
  * Released under the MIT License.
  */
 
 'use strict';
 
 module.exports = function dotfileRegex() {
-  return /(?:\/|^)(\.[^\/]*)$/;
+  return /(?:^|[\\\/])(\.(?!\.)[^\\\/]+)$/;
 };
