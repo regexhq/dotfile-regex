34da66eb9f45725f0218aaf4006e26083b4fc933
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-30 11:01:50 -0400

    add unit tests

--- test.js
+++ test.js
@@ -1,48 +1,71 @@
 /*!
  * dotfile-regex <https://github.com/regexps/dotfile-regex>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License
+ * Copyright (c) 2015-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
 
+require('mocha');
 var assert = require('assert');
-var re = require('./')();
+var regex = require('./');
 
-it('should match dotfiles', function() {
-  assert.equal(re.exec('a/b/c/d/.gitignore')[0], '/.gitignore');
-  // assert.equal(re.exec('a/b/c/d/.gitignore')[1], '.gitignore');
-  assert.equal(re.exec('a/.b/c/.gitignore')[0], '/.gitignore');
-  // assert.equal(re.exec('a/.b/c/.gitignore')[1], '.gitignore');
-  assert.equal(re.exec('a/.gitignore')[0], '/.gitignore');
-  // assert.equal(re.exec('a/.gitignore')[1], '.gitignore');
-  assert.equal(re.exec('.gitignore')[0], '.gitignore');
-  // assert.equal(re.exec('.gitignore')[1], '.gitignore');
-  assert.equal(re.exec('/.gitignore')[0], '/.gitignore');
-  // assert.equal(re.exec('/.gitignore')[1], '.gitignore');
-});
+function test(str) {
+  return regex().test(str);
+}
 
-it('should not match non-dotfiles', function() {
-  assert.equal(re.exec('a/b/c/d/e.js'), null);
-  assert.equal(re.exec('a/b.c.d/e.js'), null);
-  assert.equal(re.exec('a/b.js'), null);
-  assert.equal(re.exec('a/.b/c/a.js'), null);
-  assert.equal(re.exec('.gitignore/foo'), null);
-});
+describe('dotfile-regex', function() {
+  var notdotfile = [
+    'a/b/c/d/e.js',
+    'a/b.c.d/e.js',
+    'a/b.js',
+    'a/.b/c/a.js',
+  ].forEach(function(filepath) {
+    it('should not match non-dotfiles: ' + filepath, function() {
+      assert(!test(filepath));
+    });
+  });
 
-it('should return false when the file is not a dotfile', function() {
-  assert.equal(re.test('a/b/c/d/e.js'), false);
-  assert.equal(re.test('a/b.c.d/e.js'), false);
-  assert.equal(re.test('a/b.js'), false);
-  assert.equal(re.test('a/.b/c/a.js'), false);
-  assert.equal(re.test('.gitignore/foo'), false);
-});
+  var falsey = [
+    './foo',
+    '.git/foo',
+    '.github/contributor.md',
+    '.gitignore/foo',
+    'a/.b/c/a.js',
+    'a/.git/c/a.js',
+    'a/b.c.d/e.js',
+    'a/b.js',
+    '/../git',
+    'a/b/c/d/e.js',
+  ].forEach(function(filepath) {
+    it('should be false: ' + filepath, function() {
+      assert(!test(filepath));
+    });
+  });
 
-it('should return true when the file is a dotfile', function() {
-  assert.equal(re.test('a/b/c/d/.gitignore'), true);
-  assert.equal(re.test('a/.b/c/.gitignore'), true);
-  assert.equal(re.test('a/.gitignore'), true);
-  assert.equal(re.test('.gitignore'), true);
-  assert.equal(re.test('/.gitignore'), true);
+  var truthy = [
+    '.editorconfig',
+    '.git',
+    '.gitignore',
+    '.travis.yml',
+    '/.git',
+    '/.gitignore',
+    'a/.b/c/.gitignore',
+    'a/.gitignore',
+    'a/b.c.d/e.js/.git',
+    'a/b/c/d/.gitignore',
+  ].forEach(function(filepath) {
+    it('should be true: ' + filepath, function() {
+      assert(test(filepath));
+    });
+  });
+
+  it('should match dotfiles', function() {
+    assert.equal(regex().exec('a/b/c/d/.gitignore')[0], '/.gitignore');
+    assert.equal(regex().exec('a/.b/c/.gitignore')[0], '/.gitignore');
+    assert.equal(regex().exec('a/.gitignore')[0], '/.gitignore');
+    assert.equal(regex().exec('.gitignore')[0], '.gitignore');
+    assert.equal(regex().exec('/.gitignore')[0], '/.gitignore');
+  });
 });
